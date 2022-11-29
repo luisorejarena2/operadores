d8b4d4cf41f41c4bb937f2405a400aff938461f2
Luis <lorejarena2@hotmail.com>
2022-11-29 13:35:35 -0500

    Añadidos html y js

---
+++ operadores/secuencial.js
@@ -0,0 +1,91 @@
+// n1=parseInt(prompt('Digita el primero numero '))
+// n2=parseInt(prompt('Digita el segundo numero '))
+// n3=parseInt(prompt('Digita el tercero numero '))
+// n4=parseInt(prompt('Digita el cuarto numero '))
+
+// res1=n1+n2
+// res2=n3*n4
+
+// console.log(res1)
+// console.log(res2)
+
+
+// let precio,cantidad,pago=0
+// precio=parseFloat(prompt('Digite el precio del articulo '))
+// cantidad=parseInt(prompt('Digite la cantidad de articulos '))
+// pago=precio*cantidad
+// console.log(pago)
+
+
+// let base,altura,res=0
+// base=parseFloat(prompt('Digite la base  '))
+// altura=parseFloat(prompt('Digite la altura '))
+// res=(base*altura)/2
+// console.log(`El area del triangulo es: '${res}`)
+
+// let interes,pago=0
+// interes=(10000000*0.27)
+// pago=interes/12
+// console.log('El pago mensual debe ser de '+pago)
+
+// let salario,hora,valorHora,nombre=0
+
+// nombre=prompt('Ingrese el nombre del trabajador')
+// valorHora=parseFloat(prompt('Digite el valor de la hora '))
+// hora=parseInt(prompt('Digita el numero de horas trabajadas '))
+// salario=hora*valorHora
+// console.log('El trabajador '+nombre+' Tiene un salario de '+salario)
+
+// let Total,canNiños,canNiñas,porNiñas,porNiños=0
+// Total=canNiñas+canNiños
+// canNiñas=parseInt(prompt('Digite cuantas son niñas '))
+// canNiños=parseInt(prompt('Digite cuantos son niños '))
+// porNiñas=canNiñas*100/Total
+// porNiños=canNiños*100/Total
+// console.log('El porcentaje de niñas es '+porNiñas+'%')
+// console.log('El porcentaje de niños es '+porNiños+'%')
+
+// let costoHora,tiempoCabina,montoPago,resta=0
+
+// costoHora=1500
+// tiempoCabina=12
+// resta=Math.trunc(tiempoCabina/5)
+// montoPago=(costoHora*(tiempoCabina-resta))
+// console.log('El monto a pagar es de '+montoPago)
+
+// let seg,min,hora,tiempo,seg1,min2=0
+// tiempo=parseInt(prompt('Ingrese el tiempo en segundos '))
+// hora=Math.trunc(tiempo/3600)
+// resihora=tiempo%3600
+// min=Math.trunc(resihora/60)
+// seg1=tiempo%60
+// console.log(hora,min,seg1)
+
+let aActual,aNacimiento,edad=0
+
+aNacimiento=(prompt('Ingrese el año de su nacimiento '))
+aActual=(prompt('En que año nos encontramos '))
+
+edad=aActual-aNacimiento
+
+console.log('Su edad es de '+edad)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
